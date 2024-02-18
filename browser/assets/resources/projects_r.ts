5984969aead4889f15543bf9360f5d66f9bf595b
Leandro Gilardi <leogilardi6@gmail.com>
2024-02-18 09:35:46 -0300

    Auto-generated commit

---
+++ browser/assets/resources/projects_r.ts
@@ -0,0 +1,9 @@
+export const projects_resource = [
+    { title: 'Titulo 1', hoverText: 'Hover Text 1', coverImage: './assets/images/projects/background.jpg', id: 1 },
+    { title: 'Titulo 2', hoverText: 'Hover Text 2', coverImage: './assets/images/projects/background.jpg', id: 2 },
+    { title: 'Titulo 3', hoverText: 'Hover Text 3', coverImage: './assets/images/projects/background.jpg', id: 3 },
+    { title: 'Titulo 4', hoverText: 'Hover Text 4', coverImage: './assets/images/projects/background.jpg', id: 4 },
+    { title: 'Titulo 5', hoverText: 'Hover Text 5', coverImage: './assets/images/projects/background.jpg', id: 5 },
+    { title: 'Titulo 6', hoverText: 'Hover Text 6', coverImage: './assets/images/projects/background.jpg', id: 6 },
+    { title: 'Titulo 7', hoverText: 'Hover Text 7', coverImage: './assets/images/projects/background.jpg', id: 7 },
+]

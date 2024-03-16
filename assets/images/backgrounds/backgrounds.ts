321d648952b162d9c0adc57c6c8d840bb400d4b7
Leandro Gilardi <leogilardi6@gmail.com>
2024-03-16 09:38:13 -0300

    Auto-generated commit

---
+++ assets/images/backgrounds/backgrounds.ts
@@ -0,0 +1,7 @@
+export const backgrounds = [
+  '../assets/images/backgrounds/background.jpg',
+  '../assets/images/backgrounds/background-1.jpg',
+  '../assets/images/backgrounds/background-2.jpg',
+  '../assets/images/backgrounds/background-3.jpg',
+  '../assets/images/backgrounds/background-4.jpg',
+];
